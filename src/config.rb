9bafaf0f127c71a87c0723da3fff9c24ef18fe81
Vishal Arora <vishal@vishalarora.com>
2018-08-11 18:33:38 -0400

    Add Pat's markup to src

---
+++ src/config.rb
@@ -0,0 +1,21 @@
+require 'compass/import-once/activate'
+
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style =  :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :scss
